ff9563aa34b707c72dac1fa790184f4f0709e202
natinha <naticamposs010607@gmail.com>
2024-05-03 18:40:51 -0300

    .

--- code/script.js
+++ code/script.js
@@ -5,29 +5,25 @@ const paragrafo = document.querySelector("[data-p]")
 const input = document.querySelector("[data-input-num]")
 
 input.addEventListener("input", (evento) => {
-  let numeros = "0123456789"
-  contador = 0
-  if (evento.target.value in numeros){
-    contador += 1
-  }
-  // let contador = numeros ? numeros.length : 0;
-  // if (contador > 11){
-  //   input.maxLength = 11;
-  // }
-}
-)
+    let valor = evento.target.value;
+    valor = valor.replace(/\D/g, "");
+
+    if (valor.length === 11) {
+        const numeroFormatado = `(${valor.slice(0, 2)}) ${valor.slice(2, 7)}-${valor.slice(7, 11)}`;
+        evento.target.value = numeroFormatado;
+        evento.target.disabled = true;
+        input.style.border= "2px solid green";
+        input.style.color = "green"
+    } else {
+        evento.target.value = valor;
+    }
+});
+
 
 const validarNum = () => {
-  // let regex = /(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/
-  const numero = input.value 
-    .replace("(", "")
-    .replace(")", "")
-    .replace("-", "")
-    .replace(" ", "")
-    .replace("55", "")
-    .replace("+", "")
-  
-  divLink.innerText = 'https://wa.me/55' + numero
+  const numero = input.value
+  const numeroLimpo = numero.replace(/\D/g, "")
+  divLink.innerText = 'https://wa.me/55' + numeroLimpo
 }
 
 
