2eeaa79d10431aa472eda2d468f1cf396e860f97
natinha <naticamposs010607@gmail.com>
2024-05-15 11:35:21 -0300

    .

--- code/script.js
+++ code/script.js
@@ -2,7 +2,7 @@ const linkBtn = document.querySelector("[data-link-btn]");
 const sendBtn = document.querySelector("[data-send-btn]");
 const divLink = document.querySelector("[data-div-link]");
 const paragrafo = document.querySelector("[data-p]");
-const input = document.querySelector("[data-input-num;]")
+const input = document.querySelector("[data-input-num]")
 
 input.addEventListener("input", (evento) => {
     let valor = evento.target.value;
@@ -11,15 +11,20 @@ input.addEventListener("input", (evento) => {
     if (valor.length === 11) {
         const numeroFormatado = `(${valor.slice(0, 2)}) ${valor.slice(2, 7)}-${valor.slice(7, 11)}`;
         evento.target.value = numeroFormatado;
-        evento.target.disabled = true;
         input.style.border= "2px solid green";
         input.style.color = "green";
     } else {
         evento.target.value = valor;
+        input.style.border= "2px solid black";
+        input.style.color = "black";
     }
   });
-  
 
+input.addEventListener("click", () => {
+  divLink.style.display = 'none';
+  paragrafo.style.display = 'none';
+})
+  
 const validarNum = () => {
   const numero = input.value;
   const numeroLimpo = numero.replace(/\D/g, "");
@@ -27,10 +32,17 @@ const validarNum = () => {
 }
 
 linkBtn.addEventListener("click", () => {
-  validarNum();
-  paragrafo.innerText = "Clique no link para copiar";
-  divLink.style.display = 'flex';
-  paragrafo.style.display = 'flex';
+  const numero = input.value;
+  if (numero.length == 15){
+    validarNum();
+    paragrafo.innerText = "Clique no link para copiar";
+    divLink.style.display = 'flex';
+    paragrafo.style.display = 'flex';
+  }
+  else{
+    alert('Coloque todos os dígitos do telefone!')
+  }
+  
 })
 
 divLink.addEventListener("click", () =>{
@@ -40,13 +52,18 @@ divLink.addEventListener("click", () =>{
 })
 
 function openInNewTab(url) {
-  console.log(url);
   var win = window.open(url, '_blank');
   win.focus();
 }
 
 sendBtn.addEventListener('click', function() {
-  validarNum();
-  const url = divLink.innerText;
-  openInNewTab(url);
+  const numero = input.value;
+  if (numero.length == 15){
+    validarNum();
+    const url = divLink.innerText;
+    openInNewTab(url);
+  }
+  else{
+    alert('Coloque todos os dígitos do telefone!')
+  }
 });
